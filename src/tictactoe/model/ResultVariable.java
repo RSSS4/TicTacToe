e16ec40935416dcc54ecf4ee443055460c32a6d9
Seniv Taras <senivtar1999@gmail.com>
2017-11-15 18:41:54 +0200

    add new pictures, model-view modified

--- src/tictactoe/model/ResultVariable.java
+++ src/tictactoe/model/ResultVariable.java
@@ -1,4 +1,4 @@
-package tictactoe.view;
+package tictactoe.model;
 
 public class ResultVariable {
 
---
+++ src/tictactoe/view/AllImages.java
@@ -0,0 +1,46 @@
+package tictactoe.view;
+
+import javax.swing.ImageIcon;
+
+public class AllImages {
+
+    public static final ImageIcon three = new ImageIcon("res/buttons/three.png");
+
+    public static final ImageIcon five = new ImageIcon("res/buttons/five.png");
+
+    public static final ImageIcon seven = new ImageIcon("res/buttons/seven.png");
+
+    public static final ImageIcon pvm = new ImageIcon("res/buttons/PvM.png");
+
+    public static final ImageIcon pvp = new ImageIcon("res/buttons/PvP.png");
+
+    public static final ImageIcon settings = new ImageIcon("res/buttons/settings.png");
+
+    public static final ImageIcon o3 = new ImageIcon("res/XObuttons/O3.png");
+
+    public static final ImageIcon x3 = new ImageIcon("res/XObuttons/X3.png");
+
+    public static final ImageIcon o5 = new ImageIcon("res/XObuttons/O5.png");
+
+    public static final ImageIcon x5 = new ImageIcon("res/XObuttons/X5.png");
+
+    public static final ImageIcon o7 = new ImageIcon("res/XObuttons/O7.png");
+
+    public static final ImageIcon x7 = new ImageIcon("res/XObuttons/X7.png");
+
+    public static final ImageIcon restart = new ImageIcon("res/buttons/restart.png");
+
+    public static final ImageIcon play = new ImageIcon("res/buttons/play.png");
+
+    public static final ImageIcon close = new ImageIcon("res/buttons/close.png");
+
+    public static final ImageIcon music = new ImageIcon("res/buttons/music.png");
+
+    public static final ImageIcon nomusic = new ImageIcon("res/buttons/nomusic.png");
+
+    public static final ImageIcon mainM = new ImageIcon("res/buttons/mainM.png");
+
+    public static final ImageIcon exit = new ImageIcon("res/buttons/exit.png");
+
+    public static final ImageIcon ok = new ImageIcon("res/buttons/ok.png");
+}
