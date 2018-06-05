057abe2c52db476a28e693e63b4de0896461eccb
sokilA <andriy-sokil@mail.ru>
2018-06-05 17:36:58 +0300

    add MediumBotTest

---
+++ src/test/java/model/MediumBotTest.java
@@ -0,0 +1,33 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MediumBotTest {
+    private MediumBot mediumBot;
+    private PvMGameProcess pvMGameProcess;
+    @Before
+    public void creat(){
+        mediumBot = new MediumBot(2);
+        pvMGameProcess = new PvMGameProcess(3,2);
+    }
+
+    @Test
+    public void hitBotCloseWinAttackOn2StepsForward() {
+        Buttons buttons[][] = new Buttons[3][3];
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new Buttons(1);
+            }
+        }
+        buttons[2][0].setWho(1);
+        mediumBot.setFieldSize(3);
+        mediumBot.refreshData(buttons);
+        PvMGameProcess.setTurn(1);
+        PvMGameProcess.refreshData(buttons);
+        mediumBot.hitBot();
+        assertEquals(2,buttons[0][0].getWho());
+    }
+}
